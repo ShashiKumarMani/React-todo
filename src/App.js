0e1b429a5170e76d47df1e2386b2c4eb375b629e
Shashi Kumar M <shashikumarm052@gmail.com>
2021-09-22 20:29:00 +0530

    Update filter functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import Todo from './components/Todo';
 import Form from './components/Form';
@@ -12,58 +12,80 @@ let completed = false;
 
 function App(props) {
 
-  console.log('Component : App');
+  // console.log('<----------------------------');
+  // console.log('Component : App');
 
   const [ tasks, setTasks ] = useState([]);
 
   function addTask(name) {
-    console.log('add task');
+    // console.log('add task');
     const newTask = {'id' : 'todo' + nanoid(),  'name' : name, 'completed': false};
     taskData.push(newTask);
     setTasks([...tasks, newTask]);
+    tasks.map(task => console.log(task.id));
+    // console.log('tasks number [Add]: ',tasks.length);
+
+    if(active) filterActive();
+    if(completed) filterCompleted();
   }
 
   function removeTask(taskId) {
+    // console.log('remove task')
     let otherTasks = tasks.filter(task => task.id !== taskId);
     taskData = taskData.filter(task => task.id !== taskId);
     setTasks(otherTasks);
   }
 
   function toggleTask(taskId) {
-
+    // console.log('toggle');
     taskData  = taskData.map(task => {
-      if(task.id == taskId) {
+      if(task.id === taskId) {
+        return {...task, 'completed': !task.completed};
+      } else {
+        return task;
+      }
+    });
+    let modTasks  = tasks.map(task => {
+      if(task.id === taskId) {
         return {...task, 'completed': !task.completed};
       } else {
         return task;
       }
     });
 
-    if(active) filterActive()
-    if(completed) filterCompleted()
+    setTasks(modTasks);
 
+    if(active) filterActive();
+    if(completed) filterCompleted();
   }
 
   function filterAll() {
-    setTasks(taskData);
+    // console.log('filter all');
     active = false;
     completed = false;
+    setTasks(taskData);
   }
 
   function filterActive() {
+    // console.log('filter active')
     active = true;
     let activeTasks = taskData.filter(task => task.completed === false);
     setTasks(activeTasks);
   }
 
   function filterCompleted() {
+    // console.log('filter completed');
     completed = true;
     let completedTasks = taskData.filter(task => task.completed === true);
     setTasks(completedTasks);
   }
 
+  // console.log('tasks number : ',tasks.length);
+
   taskList = tasks.map(task => <Todo key={task.id} id={task.id} name={task.name} completed={task.completed} removeTask={removeTask} toggleTask={toggleTask}/>);    
 
+  // console.log('tasks map()');
+  // console.log('---------------------------->');
   const taskNumText = tasks.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskData.length} ${taskNumText} remaining`; 
 
