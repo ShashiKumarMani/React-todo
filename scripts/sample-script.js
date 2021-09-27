c1946928110c60d8d2df3afc5f50ad4f03117ff6
Shashi Kumar M <shashikumarm052@gmail.com>
2021-09-27 21:30:54 +0530

    Remove greeter deploy statements

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -14,13 +14,6 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
-
   const TodoList = await hre.ethers.getContractFactory('TodoList');
   const todoList = await TodoList.deploy();
 
